3fbcce2f163a00bb3913c2d666ec5f62c2c70804
Nerijus <surkeela@yahoo.com>
2021-10-20 12:51:40 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Nerijus Surkys on 2021-10-20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
